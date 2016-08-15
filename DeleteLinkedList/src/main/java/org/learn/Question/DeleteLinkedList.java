278abb8e527f92876a68e1c1cc7c848ab1c68928
makeinjava <makeinjava@gmail.com>
2016-08-15 23:23:29 +0530

    Updated code

--- DeleteLinkedList/src/main/java/org/learn/Question/DeleteLinkedList.java
+++ DeleteLinkedList/src/main/java/org/learn/Question/DeleteLinkedList.java
@@ -3,28 +3,28 @@ package org.learn.Question;
 import org.learn.List.Node;
 
 public class DeleteLinkedList {
-	
+
 	public static Node delete(Node head) {
 		Node backup = null;
-	    while(head != null) {	    	
-	    	backup = head.next;
-	    	head = null;
-	    	head = backup;
-	    }
-	    return head;
+		while (head != null) {
+			backup = head.next;
+			head = null;
+			head = backup;
+		}
+		return head;
 	}
-	
+
 	public static void insert(Node head, int data) {
-		while(head.next != null) 
+		while (head.next != null)
 			head = head.next;
 		head.next = new Node(data);
 	}
-	
+
 	public static void print(Node head) {
-		while(head != null) {
-			System.out.printf("%d ",head.data);
+		while (head != null) {
+			System.out.printf("%d ", head.data);
 			head = head.next;
 		}
-		System.out.println("");		
+		System.out.println("");
 	}
 }
